6fd4f86254d427e19405e2ee5bcad57819f079be
bhoomikajethwani <bhoomsdj@gmail.com>
2024-01-04 16:55:37 -0500

    Added Button Test, quit browser

--- src/EbaySearch.java
+++ src/EbaySearch.java
@@ -1,3 +1,4 @@
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
@@ -12,17 +13,41 @@ public class EbaySearch {
 
         driver = new ChromeDriver();
 
-        driver.get("https://www.youtube.com");
-
-
+        driver.get("https://www.ebay.com");
 
     }
 
-    public static void main(String[] args) {
+    public void searchButton() throws InterruptedException {
+        Thread.sleep(1000);
+        driver.findElement(By.id("gh-ac")).sendKeys("JBL Speakers");
+        driver.findElement(By.id("gh-btn")).click();
+        Thread.sleep(1000);
+
+        driver.findElement(By.linkText("Daily Deals")).click();
+
+    }
+
+    public void navigate() throws InterruptedException {
+        Thread.sleep(1000);
+        driver.navigate().to("https://www.youtube.com");
+        Thread.sleep(1000);
+        driver.navigate().back();
+        System.out.println("Title of page: " + driver.getTitle() );
+
+    }
+
+    public void closeBrowser() {
+        driver.quit();
+    }
+
+    public static void main(String[] args) throws InterruptedException {
         System.out.println("Hello world!");
         EbaySearch obj = new EbaySearch();
         obj.launchBrowser();
-        System.out.println("Hello world!");
+        obj.searchButton();
+        obj.navigate();
+        obj.closeBrowser();
+        System.out.println("Done!");
     }
 
 }
